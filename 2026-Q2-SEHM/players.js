d267e60e17d24061428d93565a907d953b3e58fc
changeme <1@1.com>
2026-04-12 04:24:30 -0400

    Preliminary roster for SE

--- 2026-Q2-SEHM/players.js
+++ 2026-Q2-SEHM/players.js
@@ -1,15 +1,15 @@
 // Player name mapping - edit this file to update display names
 const PLAYERS = {
-  [MAIN_ROLE_ID.DPS1]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.DPS2]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.DPS3]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.DPS4]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.DPS5]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.DPS6]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.DPS7]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.DPS8]: { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.H1]:   { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.H2]:   { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.MT]:   { name: "",   tag: "",       nickname: "", shortName: "" },
-  [MAIN_ROLE_ID.OT]:   { name: "",   tag: "",       nickname: "", shortName: "" }
+  [MAIN_ROLE_ID.DPS1]: { name: "Honeybee",   tag: "",       nickname: "", shortName: "Honeybee" },
+  [MAIN_ROLE_ID.DPS2]: { name: "Jess182",     tag: "Alkosh",       nickname: "", shortName: "Jess" },
+  [MAIN_ROLE_ID.DPS3]: { name: "FrogMasta", tag: "",        nickname: "", shortName: "Frog" },
+  [MAIN_ROLE_ID.DPS4]: { name: "?",   tag: "",  nickname: "", shortName: "?" },
+  [MAIN_ROLE_ID.DPS5]: { name: "Beagles",     tag: "",       nickname: "", shortName: "Beagles" },
+  [MAIN_ROLE_ID.DPS6]: { name: "Pandalore",   tag: "",       nickname: "", shortName: "Panda" },
+  [MAIN_ROLE_ID.DPS7]: { name: "Moxy",   tag: "", nickname: "", shortName: "Moxy" },
+  [MAIN_ROLE_ID.DPS8]: { name: "CommunistBanker",   tag: "RL",         nickname: "", shortName: "CBanker" },
+  [MAIN_ROLE_ID.H1]:   { name: "Lissa",    tag: "SPC",          nickname: "", shortName: "Lissa" },
+  [MAIN_ROLE_ID.H2]:   { name: "Violon/B",    tag: "PP",           nickname: "", shortName: "Violon" },
+  [MAIN_ROLE_ID.MT]:   { name: "Egandara",   tag: "Main Tank", nickname: "", shortName: "Egandara" },
+  [MAIN_ROLE_ID.OT]:   { name: "Sparrachion",     tag: "Off Tank",    nickname: "", shortName: "Sparr" }
 };
